a4923c53922a9d2e828d9ea5dabaab607a6daf26
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-04-16 20:22:36 +0530

    Update admin.py

--- dashboard/ATCdash/admin.py
+++ dashboard/ATCdash/admin.py
@@ -1,4 +1,3 @@
-# from django.contrib import admin
 from django.contrib.admin.models import LogEntry
 from django.contrib import admin
 from ATCdash.models import UserActivity
@@ -8,5 +7,9 @@ class UserActivityAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'login_time', 'logout_time')
 
 
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('object_repr', 'action_time', 'user', 'content_type', 'change_message')
+
+
 admin.site.register(UserActivity, UserActivityAdmin)
-admin.site.register(LogEntry)
+admin.site.register(LogEntry, LogEntryAdmin)
