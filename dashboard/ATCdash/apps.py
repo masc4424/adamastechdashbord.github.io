77c366850f3a8c10f1edf04c4d9b5d85f0a0a569
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-04-16 10:45:52 +0530

    Update apps.py

--- dashboard/ATCdash/apps.py
+++ dashboard/ATCdash/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AtcdashConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ATCdash'
+
+    def ready(self):
+        import ATCdash.signals
