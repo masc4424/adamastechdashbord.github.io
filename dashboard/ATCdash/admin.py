5694ab93c502cb2f62b18e9e907efb89fed1a570
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-04-16 10:44:42 +0530

    Update admin.py

--- dashboard/ATCdash/admin.py
+++ dashboard/ATCdash/admin.py
@@ -1,3 +1,6 @@
+from django.contrib.admin.models import LogEntry
 from django.contrib import admin
+from ATCdash.models import UserActivity
 
-# Register your models here.
+admin.site.register(UserActivity)
+admin.site.register(LogEntry)
